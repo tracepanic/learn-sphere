e7347b48c30fe8568d78a610734946917a7b87df
tracepanic <patrickscript@proton.me>
2025-03-04 19:41:26 +0300

    Fix dependencies and add prisma

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen('8000', '0.0.0.0');
 }
-bootstrap();
+
+void bootstrap();
